754045a7f80ce3f3d0fd3d8bde15500e24d04b30
Jeffrey Zelaya <jeffreyz.dev@gmail.com>
2020-05-05 14:02:53 -0600

    ready to add functionality to numbers

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import React, {useState} from 'react';
 
     // STAR MATCH - Starting Template
 
+const StarsDisplay = (props) => (
+  <>
+  {utils.range(1, props.count).map(starId => 
+    <div key={starId} className="star" />
+  )}
+
+  </>
+)
+
+const PlayNumber = (props) => (
+  <button className="number" onClick={()=> console.log('Num', props.number)}>
+    {props.number}
+    </button>
+)
+
 const App = () => {
   const [stars, setStars] = useState(utils.random(1,9))
   return (
@@ -15,14 +30,12 @@ const App = () => {
 
       <div className="body">
         <div className="left">
-          {utils.range(1, stars).map(starId => 
-            <div key={starId} className="star" />
-            )}
+          <StarsDisplay count={stars}/>
         </div>
 
         <div className="right">
           {utils.range(1, 9).map(number => 
-          <button key={number} className="number">{number}</button>
+          <PlayNumber key={number} number={number}/>
           )}
         </div>
       </div>
